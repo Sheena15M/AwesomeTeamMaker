d2c0964fe3a8e146d5907d5bfb9e956ccd7083fc
TASHENA MALLOY <SHEENA15M@yahoo.com>
2020-11-30 21:48:41 -0500

    app js updated

--- app.js
+++ app.js
@@ -1,204 +1,255 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-
-const render = require("./lib/htmlRenderer");
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+var teamName;
+const team = {manager: {}, engineers: [], interns: []};
 
-const teamMembers = [];
-const idArray = [];
 
-function initApp() {
-    startHtml();
-    addMember();
+var managerTemplate;
+var engineerTemplate;
+var internTemplate;
+var mainTemplate;
+
+init();
+
+function init() {
+    loadTemplates();
+    askTeamName();
 }
 
-function addMember() {
-    inquirer.prompt ([{
-        type: "input",
-        message: "What is the team member's name",
-        name: "name"
-},
-{
-    type: "list",
-    message: "What position does the team member hold",
-    choices: [
-        "Manager",
-        "Engineer",
-        "Intern"
-    ],
-    name: "position"
-},
-{
-    type: "input",
-    message: "What is their ID",
-    name: "ID"
-},
-{
-    type: "input",
-    message:"What is the team member's email address",
-    name: "email"
-}])
+function askTeamName() {
+    inquirer.prompt([
+        {
+            type: 'prompt',
+            name: 'name',
+            message: 'What is your team\'s name?'
+        }
+    ]).then(answers => { 
+        teamName = answers.name; 
+        askAddNewEmployee(); 
+    });
+}
 
-.then(function({name, role, id, email}) {
-    let roleInfo = "";
-    if (role ==="Engineer") {
-        roleInfo = "Github username";
-    } else if (role === "Intern") {
-        roleInfo = "school name";
-    } else {
-        roleInfo = "office phone number";
+function askAddNewEmployee() {
+
+    var choices = ["Manager","Engineer","Intern","Done"];
+
+    if (team.manager.name) {
+        choices = choices.splice(1);
     }
 
-    inquirer.prompt([{
-        type: "input",
-        message: 'What is the team members ${roleInfo}',
-        name: "roleInfo"
-    },
-    {
-        type: "list",
-        message: "Any more team members to add?",
-        choices: [
-            "yes",
-            "no"
-        ],
-        name: "additionalMembers"
-    
-    }])
-})
-    .then(function({roleInfo,additionalMembers}){
-        let newMember;
-        if (role === "Engineer") {
-            newMember = new Engineer (name, id, email, roleInfo);
-        } else if (role === "Intern") {
-            newMember = new Intern (name, id, email, roleInfo);
-        } else {
-            newMember = new Manager (name, id, email, roleInfo);
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Add an employee",
+            name: 'employeeType',
+            choices: choices
         }
-    })
-        employees.push(newMember);
-        addHtml(newMember)
-        .then(function() {
-            if (additionalMembers === "yes") {
-                addMember();
-            } else {
-                finishHtml();
-            }
+    ]).then(answers => {
+        if (answers.employeeType === "Done") {
+            renderTeamHTML();
+        } else {
+            askForDetails(answers.employeeType);
+        }
     });
 }
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+function askForDetails(type) {
 
-function startHtml() {
-    const html = `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-        <title>Team Layout</title>
-    </head>
-    <body>
-        <nav class="navbar navbar-dark bg-dark mb-5">
-            <span class="navbar-brand mb-0 h1 w-100 text-center">Team Layout</span>
-        </nav>
-        <div class="container">
-            <div class="row">`;
-    fs.writeFile("./output/team.html", html, function(err) {
-        if (err) {
-            console.log(err);
-        }
-    });
-    console.log("start");
-}
+    let questions = [
+        {
+            type: "prompt",
+            name: "name",
+            message: `Enter ${type}'s name: `
+        },
+        {
+            type: "prompt",
+            name: "title",
+            message: `Enter ${type}'s title: `
+        },
+        {
+            type: "prompt",
+            name: "email",
+            message: `Enter ${type}'s email: `
+        },
+    ];
 
-function addHtml(member) {
-    return new Promise(function(resolve, reject) {
-        const name = member.getName();
-        const role = member.getRole();
-        const id = member.getId();
-        const email = member.getEmail();
-        let data = "";
-        if (role === "Engineer") {
-            const gitHub = member.getGithub();
-            data = `<div class="col-6">
-            <div class="card mx-auto mb-3" style="width: 18rem">
-            <h5 class="card-header">${name}<br /><br />Engineer</h5>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">ID: ${id}</li>
-                <li class="list-group-item">Email Address: ${email}</li>
-                <li class="list-group-item">GitHub: ${gitHub}</li>
-            </ul>
-            </div>
-        </div>`;
-        } else if (role === "Intern") {
-            const school = member.getSchool();
-            data = `<div class="col-6">
-            <div class="card mx-auto mb-3" style="width: 18rem">
-            <h5 class="card-header">${name}<br /><br />Intern</h5>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">ID: ${id}</li>
-                <li class="list-group-item">Email Address: ${email}</li>
-                <li class="list-group-item">School: ${school}</li>
-            </ul>
-            </div>
-        </div>`;
-        } else {
-            const officePhone = member.getOfficeNumber();
-            data = `<div class="col-6">
-            <div class="card mx-auto mb-3" style="width: 18rem">
-            <h5 class="card-header">${name}<br /><br />Manager</h5>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">ID: ${id}</li>
-                <li class="list-group-item">Email Address: ${email}</li>
-                <li class="list-group-item">Office Phone: ${officePhone}</li>
-            </ul>
-            </div>
-        </div>`
-        }
-        console.log("adding team member");
-        fs.appendFile("./output/team.html", data, function (err) {
-            if (err) {
-                return reject(err);
-            };
-            return resolve();
+    if (type === "Manager") {
+        questions.push( {
+            type: "prompt",
+            name: "other",
+            message: `Enter ${type}'s office number: `
         });
-    });
-    
-            
-    
-        
-    
+    } else if (type === "Engineer") {
+        questions.push( {
+            type: "prompt",
+            name: "other",
+            message: `Enter ${type}'s GitHub username: `
+        });
+    } else if (type === "Intern") {
+        questions.push( {
+            type: "prompt",
+            name: "other",
+            message: `Enter ${type}'s school: `
+        });
+    }
+
+    inquirer.prompt(questions).then(answers => createEmployee(answers, type));
+
+} 
+
+function createEmployee(answers, type) {
+
+    if (type === "Manager") {
+        let manager = new Manager(answers.name,answers.title,answers.email,answers.other);
+        team.manager = manager;
+    } else if (type === "Intern") {
+        let intern = new Intern(answers.name,answers.title,answers.email,answers.other);
+        team.interns.push(intern);
+    } else if (type === "Engineer") {
+        let engineer = new Engineer(answers.name,answers.title,answers.email,answers.other);
+        team.engineers.push(engineer);
+    }
+
+    console.log(`Added ${type} ${answers.name} to ${teamName}`);
+
+    askAddNewEmployee();
     
 }
 
-function finishHtml() {
-    const html = ` </div>
-    </div>
-    
-</body>
-</html>`;
+function renderTeamHTML() {
 
-    fs.appendFile("./output/team.html", html, function (err) {
-        if (err) {
-            console.log(err);
-        };
+    var html = "";
+
+    if (!team.manager.name) {
+        console.log("Must have a Manager.");
+        return askAddNewEmployee();
+    }
+    if (team.interns.length === 0) {
+        console.log("Must have at least one intern.");
+        return askAddNewEmployee();
+    }
+    if (team.engineers.length === 0) {
+        console.log("Must have at least one intern");
+        return askAddNewEmployee();
+    }
+
+    html += populateTemplate(false, managerTemplate, team.manager);
+
+    team.engineers.forEach(item => {
+        html += populateTemplate(false, engineerTemplate, item);
     });
-    console.log("end");
+
+    team.interns.forEach(item => {
+        html += populateTemplate(false, internTemplate, item);
+    });
+
+    html = populateTemplate(true, mainTemplate, html);
+
+    var filename = sanitizeFilename(teamName);
+
+    fs.writeFile("./output/"+filename+".html",html,(err) => {
+        if (err) throw err;
+        console.log(`Your ${teamName} summary file has been created!`);
+    });
+
 }
 
-initApp()
+function sanitizeFilename(filename) {
+    var allowed = "abcdefghijklmnopqrstuvwxyz0123456789";
+    return filename.split('').map(item => {
+        if(allowed.indexOf(item.toLowerCase()) === -1) {
+            return '';
+        } else {
+            return item.toLowerCase();
+        }
+    }).join('');
+}
+
+function populateTemplate(main, html, details) {
+    var templateReplacements = [];
+    if (!main) {
+        templateReplacements = [
+            {
+                find: "zzzEmployeeNamezzz",
+                replace: details.getName()
+            },
+            {
+                find: "zzzEmployeeRolezzz",
+                replace: details.getRole()
+            },
+            {
+                find: "zzzEmployeeTitlezzz",
+                replace: details.getTitle()
+            },
+            {
+                find: "zzzEmployeeIdzzz",
+                replace: details.getId()
+            },
+            {
+                find: "zzzEmployeeEmailzzz",
+                replace: details.getEmail()
+            }
+        ]
+
+        if (details.getRole() === "Manager") {
+            templateReplacements.push({
+                find: "zzzEmployeeOfficezzz",
+                replace: details.getOfficeNumber()
+            });
+        } else if (details.getRole() === "Engineer") {
+            templateReplacements.push({
+                find: "zzzEmployeeGithubzzz",
+                replace: details.getGithub()
+            });
+        } else if (details.getRole() === "Intern") {
+            templateReplacements.push({
+                find: "zzzEmployeeSchoolzzz",
+                replace: details.getSchool()
+            });
+        }
+    } else {
+        templateReplacements = [{
+            find: "zzzTeamNamezzz",
+            replace: teamName
+        },{
+            find: "zzzTeamSummaryzzz",
+            replace: details
+        }]
+    }
+
+    templateReplacements.forEach(filter => {
+        var regex = new RegExp(filter.find, "g");
+        html = html.replace(regex, filter.replace);
+    });
+
+    return html;
+
+}
+
+function loadTemplates() {
+    fs.readFile("templates/manager.html", "utf8", (err,data) => {
+        if (err) return console.log(err);
+        managerTemplate = data;
+    });
+
+    fs.readFile("templates/engineer.html","utf8", (err,data) => {
+        if (err) return console.log(err);
+        engineerTemplate = data;
+    });
+
+    fs.readFile("templates/intern.html","utf8", (err,data) => {
+        if (err) return console.log(err);
+        internTemplate = data;
+    });
+
+    fs.readFile("templates/main.html","utf8",(err,data) => {
+        if (err) return console.log(err);
+        mainTemplate = data;
+    });
+} i
